de277f7a8775c587c4f686643c123e49d3fb60e2
Bruno "Kanashi <38367413+BrunoPablo@users.noreply.github.com>
2018-12-01 04:00:54 -0200

    Atualização do controller Usuario.
    Implementação da função perfil e login, modificação da função saveOrUpdate.

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -6,16 +6,70 @@
  */
 
 module.exports = {
-    
+  login: function(req, res){
+    res.view("pages/usuario/login");
+  },
 
-    cadastro: function(req, res){
-        res.view("pages/usuario/cadastro");
-    },
+  perfil: async function (req, res) {
+    var pkid = parseInt(req.param("id"));
 
-    perfil: function(req, res){
-        res.view("pages/usuario/perfil");
-    },
-    
-    
+    var dadosUsuario = await Usuario.find({
+      where: {
+        id: pkid
+      },
+      select: ['nome', 'email'],
+    });
+
+    res.view("pages/usuario/perfil", {
+      notice: req.param("notice"),
+      usuario: dadosUsuario
+    });
+
+  },
+
+
+  cadastro: function (req, res) {
+    res.view("pages/usuario/cadastro");
+  },
+
+  saveOrUpdate: function (req, res) {
+    var pkid = parseInt(req.param("id"));
+    var model = {
+      nome: req.param("nome"),
+      email: req.param("email"),
+      senha: req.param("senha")
+    }
+    if (pkid > 0) {
+      Usuario.update({
+        id: pkid
+      }, model).exec(function (err, newmodel) {
+        if (!err) {
+          res.redirect(
+            "/usuario/perfil?id=" + model.id + "&notice=Salvo_com_sucesso!"
+          );
+        } else {
+          res.redirect(
+            "/usuario/perfil?notice=Erro" + err
+
+          );
+        }
+      });
+    } else {
+      Usuario.create(model).exec(function (err, newmodel) {
+        if (!err) { //salvou
+          console.log(newmodel);
+          res.redirect(
+            "/usuario/perfil?id=" + model.id + "&notice=Salvo_com_sucesso!"
+          );
+        } else { //naosalvou
+          res.redirect(
+            "/usuario/perfil?notice=Erro" + err
+          );
+        }
+      });
+    }
+
+  },
+
+
 };
-
