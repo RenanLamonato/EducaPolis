d91e3f5e9ed8aacf81ffce029b6e41692af74430
Bruno "Kanashi <38367413+BrunoPablo@users.noreply.github.com>
2018-12-06 22:37:29 -0200

    Implementação do model Forum.js

---
+++ api/models/Forum.js
@@ -0,0 +1,55 @@
+/**
+ * Forum.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    id:{
+        type: 'number',
+        unique: true,
+        autoIncrement: true
+    },
+
+    autor:{
+        type:"string",
+        required: true,
+        minLength:1
+    },
+
+    resumo:{
+        type:"string",
+        required: true,
+        minLength: 1,
+        maxLength: 200
+    },
+
+    conteudo:{
+        type:"string",
+        required: true,
+        minLength: 1,
+        maxLength:3000,
+    },
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
