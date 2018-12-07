d4e73c47576bfd7589ae2835a6eff25f0ffba48d
Bruno "Kanashi <38367413+BrunoPablo@users.noreply.github.com>
2018-12-06 22:32:03 -0200

    Add files via upload

--- api/controllers/ForumController.js
+++ api/controllers/ForumController.js
@@ -6,7 +6,27 @@
  */
 
 module.exports = {
-  
+  novaAplicacao: function(req, res){
+    	res.view("pages/forum/novaPublicacao");
+  },
+
+
+  index:  function(req, res){
+    	res.view("pages/forum/forum");
+  },
+
+  levaID: function(req, res){
+
+    	var url = window.location.href;
+    	url = url.split('?id=');
+    	url = url[1];
+    	var pkid = parseInt(req.param("url")); 
+    	res.redirect(
+    		"/forum/index?id="+pkid+"&notice=Bem_vindo_ao_forum"
+    	);
+
+    	/*Não consegui implementar o resto*/
+  },
 
 };
 
